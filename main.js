8a6827d77a016cf089ead29cc0050992fd07a56f
Štěpán Tomsa <stomsa@redhat.com>
2020-07-01 09:52:42 +0200

    Extract DOM queries to a single object
    Created a namespace object to include all the DOM queries.

--- main.js
+++ main.js
@@ -11,59 +11,52 @@
 
 (function() {
     'use strict';
-    
+
     var _domQueries = {
-	checkboxes: function () {
+        form: function () {
+            return document.querySelector('[data-automation-id=panelSet]');
+        },
+        addButton: function (form) {
+            return form.querySelector('[data-automation-id=panelSetAddButton]')
+        },
+        entryRows: function (form) {
+            return form.querySelectorAll('[data-automation-id=panelSetRow]');
+        },
+        select: function (row) {
+            return row.querySelector('[data-automation-id=selectWidget]');
+        },
+        selectPopup: function (select) {
             var queries = [
-		'[data-automation-id=fieldSetContent]',
-		'[data-automation-id=checkboxPanel]'
+                '[data-automation-id=selectWidget-SuggestionPopup]',
+                '[data-associated-widget=\'' + select.id + '\']'
+            ];
+            var query = queries.join(',');
+            return document.querySelector(query);
+        },
+        timeInputs: function (row) {
+            return row.querySelectorAll('[data-automation-id=standaloneTimeWidget] input');
+        },
+        menuItems: function (popup) {
+            return popup.querySelectorAll('[data-automation-id=menuItem]');
+        },
+        checkboxes: function () {
+            var queries = [
+                '[data-automation-id=fieldSetContent]',
+                '[data-automation-id=checkboxPanel]'
             ], query = queries.join(' ');
             return document.querySelectorAll(query);
-	}
-    },
-	Checkboxes = function () {
-	    this.checkboxes = _domQueries.checkboxes();
-	    this.fill = function () {
-		var i = 0;
-		while (i < 5) {
-		    this.checkboxes[i].click();
-		    i++;
-		}
-	    }
-	};
+        }
+    }, _Checkboxes = function () {
+        this.checkboxes = _domQueries.checkboxes();
+        this.fill = function () {
+            var i = 0;
+            while (i < 5) {
+                this.checkboxes[i].click();
+                i++;
+            }
+        }
+    };
 
-    function _domGetForm() {
-        return document.querySelector('[data-automation-id=panelSet]');
-    }
-
-    function _domGetAddButton(form) {
-        return form.querySelector('[data-automation-id=panelSetAddButton]')
-    }
-
-    function _domGetEntryRows(form) {
-        return form.querySelectorAll('[data-automation-id=panelSetRow]');
-    }
-
-    function _domGetSelect(row) {
-        return row.querySelector('[data-automation-id=selectWidget]');
-    }
-
-    function _domGetSelectPopup(select) {
-        var queries = [
-            '[data-automation-id=selectWidget-SuggestionPopup]',
-            '[data-associated-widget=\'' + select.id + '\']'
-        ];
-        var query = queries.join(',');
-        return document.querySelector(query);
-    }
-
-    function _domGetMenuItems(popup) {
-        return popup.querySelectorAll('[data-automation-id=menuItem]');
-    }
-
-    function _domGetTimeInputs(row) {
-        return row.querySelectorAll('[data-automation-id=standaloneTimeWidget] input');
-    }
 
     function _domCreateButton(caption) {
         var button = document.createElement('button');
@@ -79,8 +72,8 @@
     }
 
     function _fillInFirstRow(row) {
-        var timeInputs = _domGetTimeInputs(row),
-            select = _domGetSelect(row),
+        var timeInputs = _domQueries.timeInputs(row),
+            select = _domQueries.select(row),
             popup, menuItems;
 
         timeInputs[0].value = '08:00';
@@ -88,31 +81,31 @@
 
 
         select.click();
-        popup = _domGetSelectPopup(select);
-        menuItems = _domGetMenuItems(popup);
+        popup = _domQueries.selectPopup(select);
+        menuItems = _domQueries.menuItems(popup);
         setTimeout(function() { menuItems[1].click(); }, 1000);
     }
 
     function _fillInSecondRow(row) {
-        var timeInputs = _domGetTimeInputs(row)
+        var timeInputs = _domQueries.timeInputs(row)
         timeInputs[0].value = '12:30';
         timeInputs[1].value = '16:30';
     }
 
 
     function _fillEverything() {
-        var form = _domGetForm(),
-            addButton = _domGetAddButton(form),
+        var form = _domQueries.form(),
+            addButton = _domQueries.addButton(form),
             rows, checkboxes, i = 0;
 
         addButton.click();
         setTimeout(function() {
-            rows = _domGetEntryRows(form);
+            rows = _domQueries.entryRows(form);
             _fillInFirstRow(rows[0]);
             _fillInSecondRow(rows[1]);
         }, 1000);
 
-        checkboxes = new Checkboxes();
+        checkboxes = new _Checkboxes();
         checkboxes.fill();
     }
 
@@ -123,3 +116,4 @@
 
     main();
 })();
+
