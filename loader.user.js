6d996a982a72f30179e77edb1fc6082b107b23a8
Glutexo <glutexo@icloud.com>
2021-11-01 21:15:08 +0100

    Add a loader Userscript
    Makes development easier. Still requires double reload though.

---
+++ loader.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Workday Timesheet Filler
+// @namespace    http://github.com/Glutexo/
+// @version      0.1
+// @description  Fills in timesheets in the Workday app.
+// @author       Glutexo
+// @match        https://wd5.myworkday.com/*
+// @grant        GM.xmlHttpRequest
+// @require      http://localhost:8000/main.user.js
+// ==/UserScript==
+
+(function() {
+   "use strict";
+
+    function onload(response) {
+        var element = document.createElement("script");
+        element.innerText = response.responseText;
+        document.body.appendChild(element);
+    }
+
+    var timestamp = Date.now(),
+        queryParam = encodeURIComponent(timestamp),
+        url = `http://localhost:8000/main.user.js?${timestamp}`;
+    GM.xmlHttpRequest({
+        method: "GET",
+        url: url,
+        onload: onload,
+    });
+})();
